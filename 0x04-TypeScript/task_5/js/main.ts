3859c98b12ec3163ee83477948f844d0056c598b
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-09 22:15:03 +0200

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -15,3 +15,13 @@ export function sumMajorCredits(
 ): MajorCredits {
   return {
     credits: subject1.cred
+
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "MinorCredits",                        
+  };
+}
